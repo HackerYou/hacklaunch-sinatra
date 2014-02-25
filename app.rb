48d0d3c4a94af0ae82f06ec065b2e5626aafb9fb
Brad Robertson <bradleyrobertson@gmail.com>
2014-02-24 21:51:15 -0500

    ex3 : ERB View with Bootstrap

--- app.rb
+++ app.rb
@@ -1,23 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  "
-    <style type='text/css'>
-      p { color: blue; }
-      .done { text-decoration: line-through; }
-      li:hover { background: #eee; }
-    </style>
-    <h1>This is an awesome heading!</h1>
-    <p>Hello World!</p>
-
-    <h3>TODO</h3>
-    <ol>
-      <li class='done'>Register Business Name</li>
-      <li class='done'>Register my-awesome-business.com</li>
-      <li>Build desk for new office space</li>
-      <li>Take Rails Course to become awesome developer</li>
-      <li>Make millions</li>
-      <li>Give millioins back to the amazing instructors that taught you :)</li>
-    </ol>
-  "
+  erb :index
 end
