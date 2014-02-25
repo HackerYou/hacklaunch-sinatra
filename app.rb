ffd0f5ea5d2bcbea800aef98d2f9f8b5235d209f
Brad Robertson <bradleyrobertson@gmail.com>
2014-02-24 21:44:30 -0500

    ex1: adding html to our app

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
 
 get '/' do
-  "Hello World!"
+  "
+    <h1>This is an awesome heading!</h1>
+    <p>Hello World!</p>
+
+    <h3>TODO</h3>
+    <ol>
+      <li>Register Business Name</li>
+      <li>Register my-awesome-business.com</li>
+      <li>Build desk for new office space</li>
+      <li>Take Rails Course to become awesome developer</li>
+      <li>Make millions</li>
+      <li>Give millioins back to the amazing instructors that taught you :)</li>
+    </ol>
+  "
 end
