2d338066669a7c5c18aed4f4ad6193162ef49815
Brad Robertson <bradleyrobertson@gmail.com>
2014-02-24 21:47:31 -0500

    ex2 : CSS To our App

--- app.rb
+++ app.rb
@@ -2,13 +2,18 @@ require 'sinatra'
 
 get '/' do
   "
+    <style type='text/css'>
+      p { color: blue; }
+      .done { text-decoration: line-through; }
+      li:hover { background: #eee; }
+    </style>
     <h1>This is an awesome heading!</h1>
     <p>Hello World!</p>
 
     <h3>TODO</h3>
     <ol>
-      <li>Register Business Name</li>
-      <li>Register my-awesome-business.com</li>
+      <li class='done'>Register Business Name</li>
+      <li class='done'>Register my-awesome-business.com</li>
       <li>Build desk for new office space</li>
       <li>Take Rails Course to become awesome developer</li>
       <li>Make millions</li>
