caa477247578ccb5c650b7f6c9854afc8a1e849a
Brad Robertson <bradleyrobertson@gmail.com>
2014-03-15 14:37:34 -0400

    gravatar

--- app.rb
+++ app.rb
@@ -1,10 +1,13 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'gravatar-ultimate'
 
 get '/' do
   erb :index
 end
 
 post '/submitted' do
-  erb :thanks
+  url = Gravatar.new(params[:email]).image_url
+
+  erb :thanks, locals: {image_url: url}
 end
